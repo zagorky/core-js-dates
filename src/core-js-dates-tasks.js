16d11429d002e6d093ffd53b071f13c227980439
zagorky <zagorkiynahyi@gmail.com>
2025-02-25 21:43:14 +0300

    feat: add 3 solutions

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -49,9 +49,20 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const weekDay = {
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+    0: 'Sunday',
+  };
+  return weekDay[new Date(date).getDay()];
 }
+
+// console.log(getDayName('01 Jan 1970 00:00:00 UTC'));
 
 /**
  * Returns the date of the next Friday from a given date.
