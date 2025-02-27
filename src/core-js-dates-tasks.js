5c34acf69de123f5fcd70efec9381b824e72a035
Daria <156232667+zagorky@users.noreply.github.com>
2025-02-27 09:59:59 +0000

    feat: add 8 solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -140,8 +140,16 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'numeric',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true,
+  }).format(new Date(date));
 }
 
 /**
