68288b2a6149a98eb61db4535f6f66eb5821c1a7
Daria <156232667+zagorky@users.noreply.github.com>
2025-02-27 13:20:11 +0000

    feat: add 9 solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -164,8 +164,10 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  return Array.from({ length: new Date(year, month, 0).getDate() }, (_, i) =>
+    new Date(year, month - 1, i + 1).getDay()
+  ).filter((day) => day === 0 || day === 6).length;
 }
 
 /**
