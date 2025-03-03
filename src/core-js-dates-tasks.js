4e6628a76d3a176ae18670a95501fb18716a0b2d
zagorky <zagorkiynahyi@gmail.com>
2025-03-03 19:03:10 +0300

    feat: add 13 solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -266,8 +266,9 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = new Date(date).getUTCFullYear();
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
 
 module.exports = {
